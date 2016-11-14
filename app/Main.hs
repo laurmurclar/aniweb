78c685e1f4d9cc991982231a58c51f335494a184
Laura Murphy-Clarkin <laurmurclar@gmail.com>
2016-11-14 22:32:40 +0000

    🤔Use blaze-svg example with scotty

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Web.Scotty as S
+import Web.Scotty as W
 
 import Data.Text.Lazy
 import Lib
 
+import Text.Blaze.Svg11 ((!), mkPath, rotate, l, m)
+import qualified Text.Blaze.Svg11 as S
+import qualified Text.Blaze.Svg11.Attributes as A
+import Text.Blaze.Svg.Renderer.String (renderSvg)
+
 main = scotty 3000 $ do
   get "/:shape" $ do
-      shape <- S.param "shape"
-      S.html $ longresponse shape
+    let a = renderSvg svgDoc
+    html $ pack a
+
+svgDoc :: S.Svg
+svgDoc = S.docTypeSvg ! A.version "1.1" ! A.width "150" ! A.height "100" ! A.viewbox "0 0 3 2" $ do
+  S.g ! A.transform makeTransform $ do
+    S.rect ! A.width "1" ! A.height "2" ! A.fill "#008d46"
+    S.rect ! A.width "1" ! A.height "2" ! A.fill "#ffffff"
+    S.rect ! A.width "1" ! A.height "2" ! A.fill "#d2232c"
+    S.path ! A.d makePath
+
+makePath :: S.AttributeValue
+makePath = mkPath $ do
+  l 2 3
+  m 4 5
+
+makeTransform :: S.AttributeValue
+makeTransform = rotate 50
