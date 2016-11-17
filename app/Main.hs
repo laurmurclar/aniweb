d143f991c9b8eeb953556bd0cc171254ace75b02
Laura Murphy-Clarkin <laurmurclar@gmail.com>
2016-11-17 14:56:15 +0000

    👀  Create Svg from a string

--- app/Main.hs
+++ app/Main.hs
@@ -5,28 +5,20 @@ import Data.Text.Lazy
 import Lib
 
 import Text.Blaze.Internal as I
-import Text.Blaze.Svg11 ((!), mkPath, rotate, l, m)
+import Text.Blaze.Svg11 ((!), rotate)
 import qualified Text.Blaze.Svg11 as S
 import qualified Text.Blaze.Svg11.Attributes as A
 import Text.Blaze.Svg.Renderer.String (renderSvg)
 
-main = scotty 3000 $ do
+main = scotty 3000 $
   get "/" $ do
     let a = renderSvg svgDoc
-    html $ (append (append "<div class='hello' style='width: 100%; height: 100%;'>" (pack a)) "</div>")
+    html $ append (append "<div class='hello' style='width: 100%; height: 100%;'>" (pack a)) "</div>"
 
 svgDoc :: S.Svg
-svgDoc = S.docTypeSvg `withAttributes` [(A.version "1.1"), (A.width "100%"), (A.height "100%"), (A.viewbox "0 0 3 2")] $ do
-  S.g $ do
-    draw [(Rect, ((style (Fill "#008d46")) ++ [(A.transform makeTransform)])), (Rect, (style (Fill "#000000"))), (Rect, (style ((Fill "#FF0000") *** (Stroke "#00FFFF"))))]
-
-makePath :: S.AttributeValue
-makePath = mkPath $ do
-  l 2 3
-  m 4 5
-
-makeTransform :: S.AttributeValue
-makeTransform = rotate (- 50)
+svgDoc = S.docTypeSvg `withAttributes` [A.version "1.1", A.width "100%", A.height "100%", A.viewbox "0 0 3 2"] $
+  S.g $
+    draw (read "[(Rect, Fill \"#FF0000\" :*** Stroke \"#00FF00\", Rotate 50.0 :<+> Scale 0.5)]")
 
 ------------ COOL STUFF ----------------
 ----------------------------------------
@@ -34,35 +26,58 @@ withAttributes :: I.Attributable a => a -> [S.Attribute] -> a
 h `withAttributes` [] = h ! A.width "1" ! A.height "1"
 h `withAttributes` (x:xs) = (h ! x) `withAttributes` xs
 
--- TODO strokeWidth
+-- TODO revise if these would be better as diff types eg. Double
+-- TODO add more style options
 data Style = IdentityS
         | Fill String
         | Stroke String
-        | ComposeS Style Style
-          deriving Read
-
-fill = Fill
-stroke = Stroke
-s0 *** s1 = ComposeS s0 s1
+        | StrokeWidth String
+        | Style :*** Style
+          deriving (Read, Show)
 
 style :: Style -> [Attribute]
 style s = styleR s []
 
 styleR :: Style -> [Attribute] -> [Attribute]
+styleR IdentityS xs = xs
 styleR (Fill c) xs = xs ++ [A.fill (stringValue c)]
 styleR (Stroke c) xs = xs ++ [A.stroke (stringValue c)]
-styleR (ComposeS s0 s1) xs = (styleR s1 (styleR s0 xs))
+styleR (StrokeWidth w) xs = xs ++ [A.strokeWidth (stringValue w)]
+styleR (s0 :*** s1) xs = styleR s1 (styleR s0 xs)
 
-type Drawing = [(Shape,[Attribute])]
+type Drawing = [(Shape, Style, Transform)]
 
 draw :: Drawing -> S.Svg
 draw [d] = shapeToDrawable d
 draw (d:ds) = shapeToDrawable d >> draw ds
 
-shapeToDrawable :: (Shape, [Attribute]) -> S.Svg
-shapeToDrawable (s, a) = (shape s) `withAttributes` a
+shapeToDrawable :: (Shape, Style, Transform) -> S.Svg
+shapeToDrawable (sh, st, tr) = shape sh `withAttributes` (style st ++ transform tr)
 
 data Shape = Rect
+              deriving Read
 
 shape :: Shape -> S.Svg
 shape Rect = S.rect
+
+-- TODO translate
+-- TODO decide on cons or concat
+-- TODO HOF
+-- TODO add more transform options
+data Transform = IdentityT
+           | Scale Double
+           | Rotate Double
+           | Transform :<+> Transform
+             deriving Read
+
+transform :: Transform -> [Attribute]
+transform t = transformR t []
+
+transformR :: Transform -> [Attribute] -> [Attribute]
+transformR IdentityT xs = xs
+transformR (Rotate r) xs = xs ++ [A.transform (S.rotate r)]
+transformR (Scale f) xs = xs ++ [A.transform (S.scale f f)]
+transformR (t0 :<+> t1) xs = transformR t1 (transformR t0 xs)
+
+-- TODO read it in from the webpage
+-- TODO break into lib files
